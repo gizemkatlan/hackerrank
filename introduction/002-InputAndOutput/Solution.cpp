7619c24f7f7a6f00b1fa2c1590630436c291f639
Gizem <ARBYL-EI.NT_Project@arcelik.com>
2019-10-20 17:30:23 +0300

    Hackerrank input and output solution

---
+++ introduction/002-InputAndOutput/Solution.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    short int a,b,c;
+    start:
+    cin >> a >> b >> c;
+    if ((a<1)||(a>1000) || (b<1)||(b>1000) || (c<1)||(c>1000))
+    {
+        cout << "Please enter the integers 1 through 1000" << endl;
+        goto start;
+    }
+    cout << a << " " << b << " " << c << endl;
+    cout << a+b+c << endl;
+    return 0;
+}
